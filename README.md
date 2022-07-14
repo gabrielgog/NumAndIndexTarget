acb6cb11e283dcbe1763469f489866c136a267e8
gabrielgog <gabrielgog96@gmail.com>
2022-07-14 18:14:50 +0100

    function for checking two numbers that adds up to the target

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NumAndIndexTarget
+
+#The function takes an array of integers and a target number. 
+
+#To test locally in your terminal or online ide run 'node index.js' after navigating into the folder/file
+
+#The function is expecting a list of numbers and the target as a parameter 
---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const check = (List, target) => {
+	for (let i = 0; i < List.length; i++) {
+		let first = List[i];
+		for (let j = 0; j < List.length; j++) {
+			if (first + List[j] === target) {
+				return `${first} ${List[j]}`;
+			}
+		}
+	}
+};
+
+
+// only change the index and the target when calling the function
+
+console.log(check([1,2,3,4,5,6,7,8,9,10], 9))
